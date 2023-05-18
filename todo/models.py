fbcdf8facf1a9e524a7ecadeaac9ea1578a4cc56
Tonywilson1211 <>
2023-05-18 15:57:04 +0000

    commit

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    # null = prevents items from being created without a name progmmatically
+    # blank = makes the field required on forms
+    # default = false by default (todo items)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
